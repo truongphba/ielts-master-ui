48bf7f38f5a478399c6657c9e58646ac0bb2c923
truongphba <truongph.ba@gmail.com>
2020-08-17 23:27:17 +0700

    create project

---
+++ src/router/routes.js
@@ -0,0 +1,32 @@
+
+const routes = [
+  { path: '/', component: () => import('pages/admin/login.vue') },
+  {
+    path: '/',
+    component: () => import('layouts/MainLayoutAdmin.vue'),
+    children: [
+      { path: '/dashboard', component: () => import('pages/admin/dashboard.vue') },
+      { path: '/dashboard_v2', component: () => import('pages/admin/dashboard_v2.vue') },
+      { path: '/dashboard_v3', component: () => import('pages/admin/dashboard_v3.vue') },
+      { path: '/customer_management', component: () => import('pages/admin/customer_management.vue') },
+      { path: '/change_request', component: () => import('pages/admin/change_request.vue') },
+      { path: '/my_profile', component: () => import('pages/admin/my_profile.vue') },
+      { path: '/sales_invoices', component: () => import('pages/admin/sales_invoices.vue') },
+      { path: '/quotes', component: () => import('pages/admin/quotes.vue') },
+      { path: '/transactions', component: () => import('pages/admin/transactions.vue') },
+      { path: '/employee_salary_list', component: () => import('pages/admin/employee_salary_list.vue') },
+      { path: '/calendar', component: () => import('pages/admin/calendar.vue') },
+      { path: '/department', component: () => import('pages/admin/department.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/admin/Error404.vue')
+  })
+}
+
+export default routes
