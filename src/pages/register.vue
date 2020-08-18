54996635979507bd60eb06d4dd0489a89bc0b742
truongphba <truongph.ba@gmail.com>
2020-08-19 03:03:06 +0700

    update

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -3,59 +3,65 @@
     <q-page-container class="absolute-center">
       <div class="q-pa-md row items-start q-gutter-md">
         <q-card class="my-card flex-center">
-          <img src="https://www.britishcouncil.vn/sites/default/files/ielts_red_-_new_ielts_landing_page_preview_image.png">
+          <img
+            src="https://www.britishcouncil.vn/sites/default/files/ielts_red_-_new_ielts_landing_page_preview_image.png">
           <q-card-section>
             <div class="text-h6">Register</div>
           </q-card-section>
           <q-card-section class="q-pt-none">
+            <div class="error" v-if="errors && errors.length">
+           <span v-for="(err, index) in errors" :key="index">
+               {{ err }}
+           </span>
+              <hr>
+            </div>
             <q-form
-              @submit="onSubmit"
               class="q-gutter-md"
             >
-             <div class="row">
-               <div class="col">
-                 <q-input
-                   filled
-                   v-model="name"
-                   label="Username *"
-                 />
-               </div>
-               <div class="col">
-                 <q-input
-                   filled
-                   v-model="email"
-                   label="Email *"
-                 />
-               </div>
-             </div>
               <div class="row">
                 <div class="col">
                   <q-input
                     filled
-                    v-model="fullName"
+                    v-model="user.name"
+                    label="Username *"
+                  />
+                </div>
+                <div class="col">
+                  <q-input
+                    filled
+                    v-model="user.email"
+                    label="Email *"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <q-input
+                    filled
+                    v-model="user.full_name"
                     label="Full Name"
                   />
                 </div>
                 <div class="col">
                   <q-input
                     filled
-                    v-model="age"
+                    v-model="user.age"
                     label="Age"
                   />
                 </div>
               </div>
-              <q-input v-if="isLecturer"
-                v-model="certificate"
-                filled
-                type="textarea"
-                label="Certificate *"
+              <q-input v-if="user.is_lecture"
+                       v-model="user.certificate"
+                       filled
+                       type="textarea"
+                       label="Certificate *"
               />
               <div class="row">
                 <div class="col">
                   <q-input
                     filled
                     type="password"
-                    v-model="password"
+                    v-model="user.password"
                     label="Password *"
                   />
                 </div>
@@ -63,20 +69,19 @@
                   <q-input
                     filled
                     type="password"
-                    v-model="rePassword"
                     label="Confirm Password *"
                   />
                 </div>
               </div>
               <q-toggle
-                v-model="isLecturer"
+                v-model="user.is_lecture"
                 checked-icon="check"
                 color="green"
                 unchecked-icon="clear"
                 label="Register as lecturer"
               />
               <div>
-                <q-btn label="Register" type="submit" color="primary"/>
+                <q-btn label="Register" type="submit" color="primary" @click="register"/>
                 <q-btn label="Back" to="/" color="primary" flat class="q-ml-sm"/>
               </div>
             </q-form>
@@ -87,16 +92,42 @@
   </q-layout>
 </template>
 <script>
+  import axios from 'axios';
   export default {
     data() {
       return {
-        isLecturer: false,
+        user: {
+          is_lecture: false
+        },
+        errors: []
+      }
+    },
+    methods: {
+      register() {
+        axios.post('http://127.0.0.1:8000/api/register', {
+          name: this.user.name,
+          email: this.user.email,
+          password: this.user.password,
+          age: this.user.age,
+          certificate: this.user.certificate,
+          is_lecture: this.user.is_lecture
+        })
+          .then(response => {
+            console.log(response.data.user)
+          })
+          .catch(error => {
+            this.errors = error.response.data.errors.email
+          })
       }
     }
   }
 </script>
 
 <style scoped>
+  .error span {
+    color: red;
+  }
+
   .login-bg {
     background: rgb(238, 174, 202);
     background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%);
